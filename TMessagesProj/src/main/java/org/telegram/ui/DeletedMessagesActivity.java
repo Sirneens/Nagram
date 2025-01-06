83ddbcd9580c6c3015758ca73e7c75b2d0af4f71
Sirneens <24855705+Sirneens@users.noreply.github.com>
2025-01-07 02:03:22 +0330

    Update DeletedMessagesActivity.java

--- TMessagesProj/src/main/java/org/telegram/ui/DeletedMessagesActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DeletedMessagesActivity.java
@@ -2,11 +2,17 @@ package org.telegram.ui;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import org.telegram.messenger.DeletedMessagesManager;
+import org.telegram.messenger.DeletedMessagesAdapter;
 import org.telegram.messenger.database.entities.DeletedMessage;
 import java.util.List;
 
 public class DeletedMessagesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private DeletedMessagesAdapter adapter;
     private DeletedMessagesManager deletedMessagesManager;
 
     @Override
@@ -14,11 +20,23 @@ public class DeletedMessagesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deleted_messages);
 
+        recyclerView = findViewById(R.id.deletedMessagesRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         deletedMessagesManager = new DeletedMessagesManager(this);
 
         // Example: Get deleted messages for account ID 1
         List<DeletedMessage> deletedMessages = deletedMessagesManager.getDeletedMessagesForAccount(1);
 
-        // TODO: Display the deleted messages in a RecyclerView or ListView
+        if (deletedMessages.isEmpty()) {
+            findViewById(R.id.emptyStateTextView).setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.emptyStateTextView).setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+
+            adapter = new DeletedMessagesAdapter(this, deletedMessages);
+            recyclerView.setAdapter(adapter);
+        }
     }
 }
